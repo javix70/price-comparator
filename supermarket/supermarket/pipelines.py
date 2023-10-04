245faf553565fc7cba8c1e6a4dbf04cb95f6f5ca
Javier pacheco <javier.pacheco@niufoods.cl>
2023-10-04 01:02:00 -0300

    [CONFIG]: Use django model and generate first pipeline

--- supermarket/supermarket/pipelines.py
+++ supermarket/supermarket/pipelines.py
@@ -5,9 +5,22 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+# from itemadapter import ItemAdapter
+from .items import *
+from products.models import *
+import asyncio
+from asgiref.sync import sync_to_async
 
+class ProductPipeline:
+    @sync_to_async
+    def _save_item(self, item):
+        product = Product(
+            gtin13=item['gtin13'],
+            brand=item['brand'],
+        )
+        product.save()
 
-class SupermarketPipeline:
-    def process_item(self, item, spider):
+    async def process_item(self, item, spider):
+        if isinstance(item, ProductItem):
+            await self._save_item(item)
         return item
